9fa54cf04b63e40f7185dccd1fcb2c35098ee878
Ziyad A <zakchouc@student.42.fr>
2024-03-02 20:09:23 +0100

    progress gnl...

--- get_next_line.c
+++ get_next_line.c
@@ -65,22 +65,43 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 int	ft_count_until_newline(char *str)
 {
-	
+	int	i;
+
+	i = -1;
+	if (!str)
+		return (0);
+	while (str[++i])
+	{
+		if (str[i] == '\n')
+			return (i + 1);
+	}
 	return (0);
 }
 
-// int	ft_read_line()
-// {
-
-// 	return (0);
-// }
+int	ft_read_line(int fd, char *line)
+{
+	if (line[0] != '0')
+		return (1);
+	if (read(fd, line, BUFFER_SIZE) > 0)
+		return (1);
+	return (0);
+}
 
 char	*get_next_line(int fd)
 {
-	char			*line;
-	static char		*static_buf = "";
+	char		*line;
+	static char	static_buf[BUFFER_SIZE];
+	int			newline_found;
 
-
+	line = NULL;
+	while ((!ft_count_until_newline(line)) && ft_read_line(fd, (char *)static_buf) > 0)
+	{
+		newline_found = ft_count_until_newline(static_buf);
+		line = ft_strnjoin(line, static_buf, newline_found);
+		if (!line)
+			return (NULL);
+		ft_memmove(static_buf, &static_buf[newline_found], newline_found);
+	}
 	return (line);
 }
 
