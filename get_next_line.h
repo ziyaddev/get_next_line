e919a3ccc43d8027631a3aea8116cc4ab1b6cc4a
Ziyad A <zakchouc@student.42.fr>
2024-03-02 20:09:03 +0100

    add some functions

--- get_next_line.h
+++ get_next_line.h
@@ -28,7 +28,7 @@ char	*get_next_line(int fd);
 
 size_t	ft_strlen(const char *str);
 
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strnjoin(char *s1, char *s2, size_t n);
 
 char	*ft_strchr(const char *s, int c);
 
@@ -42,6 +42,8 @@ char	*ft_strcpy(char *dest, const char *src);
 
 int		ft_count_until_newline(char *str);
 
-// int	ft_read_line();
+void	*ft_memmove(void *dest, const void *src, size_t n);
+
+int		ft_read_line(int fd, char *line);
 
 #endif
